8af420f5cce53b504cc64219d9d7445d4091ac7e
mnunezve <mnunezve@gmail.com>
2024-07-09 07:10:31 +0000

    Cursor effect

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,29 +11,34 @@ import CharacterCards from "./views/CharactersCards";
 import CharacterDetails from "./views/CharacterDetails";
 import StarshipDetails from "./views/StarshipDetails";
 import NotFoundPage from "./views/NotFoundPage";
+import NeonCursorEffect from "./component/NeonCursorEffect";
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/characterdetails/:uid" element={<CharacterDetails />} />
-						<Route path="/starshipdetails/:uid" element={<StarshipDetails />} />
-						<Route path="*" element={<NotFoundPage />} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <NeonCursorEffect />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/characterdetails/:uid"
+              element={<CharacterDetails />}
+            />
+            <Route path="/starshipdetails/:uid" element={<StarshipDetails />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
