5f905e5eb07176cc24d709f67a3837e84650fdb8
mnunezve <mnunezve@gmail.com>
2024-07-11 15:49:00 +0000

    Update

--- src/js/component/BackgroundMusic.js
+++ src/js/component/BackgroundMusic.js
@@ -4,19 +4,18 @@ import TheImperialMarch from "../../sound/the-imperial-march.mp3";
 const BackgroundMusic = () => {
   useEffect(() => {
     const BackgroundAudio = new Audio(TheImperialMarch);
-    BackgroundAudio.loop = true; // Agregar esta línea si quieres que la música se repita
+    BackgroundAudio.loop = true; 
     BackgroundAudio.play().catch((error) => {
       console.error("Error playing the audio file:", error);
     });
 
-    // Cleanup function to pause the audio when the component unmounts
     return () => {
       BackgroundAudio.pause();
-      BackgroundAudio.currentTime = 0; // Reiniciar el audio al desmontar
+      BackgroundAudio.currentTime = 0; 
     };
-  }, []); // Empty dependency array to run once on mount
+  }, []);
 
-  return null; // No mostrar ningún contenido
+  return null; 
 };
 
 export default BackgroundMusic;
